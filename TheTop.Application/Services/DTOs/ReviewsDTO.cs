79f528660ef95d51aea8aa69cb47b60dbac3d1c1
moamenhredeen <moamenhredeen@gmail.com>
2021-10-06 11:05:22 +0200

    add autherization, fix project names

---
+++ TheTop.Application/Services/DTOs/ReviewsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheTop.Application.Services.DTOs
+{
+    class ReviewsDTO
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Massage { get; set; }
+        public bool IsActiv { get; set; }
+        public int UserId { get; set; }
+    }
+}
